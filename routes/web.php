a76136742a2d48fdd5997b903fef7a2f4b3a3bcb
Marcos <Marcoscabet@gmail.com>
2024-05-16 23:06:04 +0200

    -proyecto laravel
    -autentificacion con fortify

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//  Pide que el correo electrónico sea verificado
+Route::get('/home', function () {
+    return view('auth.dashboard');
+})->middleware(['auth', 'verified']);
