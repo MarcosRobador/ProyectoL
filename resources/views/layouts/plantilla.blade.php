cb01ae6c97ce320836f3e272e9722d829897a535
Marcos <Marcoscabet@gmail.com>
2024-05-23 13:58:45 +0200

    estilos

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -12,47 +12,64 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css">
     <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
 </head>
 
 <body>
     <header>
-        <nav>
-            <div class="logo">
-                <a href="#">
-                    <img src="{{ asset('images/logo.png') }}" alt="Logo" class="logo-img">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('images/logo.png') }}" alt="Logo" style="width: 60px; height: auto;">
                 </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        @if (auth()->check())
+                            @if (auth()->user()->hasRole('admin'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('zapatillas.index') }}">Zapatillas</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('zapatillas.create') }}">Crear</a>
+                                </li>
+                            @endif
+                            <li class="nav-item">
+                                <a class="nav-link btn btn-danger text-white" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Log in</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                                </li>
+                            @endif
+                        @endif
+                    </ul>
+                </div>
             </div>
-            <ul>
-                @if (auth()->check())
-                    @if (auth()->user()->hasRole('admin'))
-                        <li><a href="{{ route('zapatillas.index') }}">Zapatillas</a></li>
-                        <li><a href="{{ route('zapatillas.create') }}">Crear</a></li>
-                    @endif
-                    <li>
-                        <a href="{{ route('logout') }}" class="btn btn-danger"
-                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                           Logout
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </li>
-                @endif
-                @yield('contentHeader')
-            </ul>
         </nav>
     </header>
 
-    <div class="container">
+    <div class="container mt-4">
         @yield('content')
     </div>
 
-    <footer>
+    <footer class="bg-dark text-white text-center py-3">
         <p>&copy; 2024 Mi Empresa. Todos los derechos reservados.</p>
     </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-sInkw3tQbJfKSOE2JToIkvbnSaQNJ5/ohcYb/sUpK5CW5qR9vvhd96lOAfA7wErf" crossorigin="anonymous"></script>
 </body>
 </html>
